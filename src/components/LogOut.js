32f9e1b37ae716af14d59a6b8ef38820ee0aa27d
mon33k <moniquemojica@ac.c4q.nyc>
2020-10-29 14:00:34 -0400

    finished todo app

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -2,28 +2,39 @@ import React from "react";
 import firebase from '../firebase';
 import {Redirect} from 'react-router-dom'
 
-const LogOut = () => {
-
-    const handleLogOutUser = (e) => {
-        e.preventDefault()
-        firebase.auth().signOut()
-            .then(() =>{
-                console.log("user signed off")
-                return <Redirect to="/" /> //***fix the logout page so that it disappears when youre routed back to the landing page */
-            })
-            .catch((error) => {
-                console.log("error --> ", error)
-            })
-        
+class LogOut extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            loggedOut: false
+        }
+        this.handleLogOutUser = this.handleLogOutUser.bind(this)
     }
 
-    return (
-        <div>
-            <div>Logout</div>
-            <button type="submit" onClick={handleLogOutUser}>LogOust</button>
-        </div>
-        
-    )
+
+    handleLogOutUser(){
+        firebase.auth().signOut().then(() => {
+                this.setState({
+                    loggedOut: true
+                })
+        }) 
+            .catch((error) => {
+                console.log("error --> ", error)
+        })
+    }
+
+    render() {
+        if(this.state.loggedOut) {
+            return <Redirect to="/" />
+        } else {
+            return (
+                <div>
+                    <div>Logout</div>
+                    <button type="submit" onClick={this.handleLogOutUser}>LogOust</button>
+                </div>
+            )
+        }
+    }
 }
 
 export default LogOut;
